1dd6ddd2cb89e97ac08f10ef4b2379e940163d5c
Aditi Thakkar <aditithakkar1998@gmail.com>
2022-04-05 16:08:07 -0400

    Final Commit

---
+++ SearchingAndSortingPrograms/src/main/java/com/company/java/SelectionSortExample2.java
@@ -0,0 +1,27 @@
+package com.company.java;
+
+import java.util.Scanner;
+
+public class SelectionSortExample2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int size, i, j, temp;
+		int arr[] = new int[50];
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.print("Enter Array Size: ");
+		size = scan.nextInt();
+		
+		System.out.print("Enter Array Elements: ");
+		for(i=0; i<size;i++) {
+			arr[i] = scan.nextInt();
+		}
+		
+		System.out.print("Sorting Array using Selection Sort Technique..\n");
+		for(i=0; )
+		
+	}
+
+}
